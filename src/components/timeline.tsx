b62d2a264eca1e73c3a89ffe9c24a1a4738a2248
genre0928 <87413846+genre0928@users.noreply.github.com>
2025-03-28 19:18:40 +0900

    트위터 삭제 기능 추가

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,4 +1,10 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { db } from "../firebase";
@@ -13,31 +19,60 @@ export interface ITweet {
   createAt: number;
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+`;
 
 export default function Timeline() {
   const [tweets, setTweet] = useState<ITweet[]>([]);
-  const fetchTweets = async () => {
-    const tweetsQuery = query(
-      collection(db, "tweet"),
-      orderBy("createAt", "desc")
-    );
-    const snapshot = await getDocs(tweetsQuery);
-    const tweets = snapshot.docs.map((doc) => {
-      const { tweet, createAt, userId, username, photo } = doc.data();
-      return {
-        tweet,
-        createAt,
-        userId,
-        username,
-        photo,
-        id: doc.id,
-      };
-    });
-    setTweet(tweets);
-  };
+
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, "tweet"),
+        orderBy("createAt", "desc"),
+        limit(25)
+      );
+      // const snapshot = await getDocs(tweetsQuery);
+      // const tweets = snapshot.docs.map((doc) => {
+      //   const { tweet, createAt, userId, username, photo } = doc.data();
+      //   return {
+      //     tweet,
+      //     createAt,
+      //     userId,
+      //     username,
+      //     photo,
+      //     id: doc.id,
+      //   };
+      // });
+      unsubscribe = onSnapshot(tweetsQuery, (snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          const { tweet, createAt, userId, username, photo } = doc.data();
+          return {
+            tweet,
+            createAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
+          };
+        });
+        setTweet(tweets);
+      });
+    };
     fetchTweets();
+    return () => {
+      unsubscribe && unsubscribe();
+    };
   }, []);
-  return <Wrapper>{tweets.map((tweet) => <Tweet key={tweet.id} {...tweet} />)}</Wrapper>;
+  return (
+    <Wrapper>
+      {tweets.map((tweet) => (
+        <Tweet key={tweet.id} {...tweet} />
+      ))}
+    </Wrapper>
+  );
 }
--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import { ITweet } from "./timeline";
 import { getRelativeTime } from "./getrelativetime";
+import { auth, db, storage } from "../firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
 
 const Wrapper = styled.div`
   display: grid;
@@ -28,18 +31,53 @@ const Payload = styled.p`
 
 const Time = styled.p`
   font-weight: 600px;
-  margin-top: 50px;
+  margin-top: 30px;
+  margin-bottom: 15px;
 `;
 
-export default function Tweet({ username, photo, tweet, createAt }: ITweet) {
-    const diffTime = getRelativeTime(createAt)
+const DeleteButton = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
 
+export default function Tweet({
+  username,
+  photo,
+  tweet,
+  createAt,
+  userId,
+  id,
+}: ITweet) {
+  const diffTime = getRelativeTime(createAt);
+  const user = auth.currentUser;
+  const onDelete = async () => {
+    const ok = confirm("정말 삭제하시겠습니까?")
+    if (!ok || user?.uid !== userId) return;
+    try {
+      await deleteDoc(doc(db, "tweet", id));
+      if (photo) {
+        const photoRef = ref(storage, `tweets/${user.uid}-${username}/${id}`);
+        await deleteObject(photoRef);
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+    }
+  };
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
         <Time>{diffTime}</Time>
+        {user?.uid === userId ? <DeleteButton onClick={onDelete}>Delete</DeleteButton> : null}
       </Column>
       {photo ? (
         <Column>
